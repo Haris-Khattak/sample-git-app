3613030fdfb967b3b04dad4b75fc560a6e077772
Muhammad Haris <muhammadharis.ktk345@gmail.com>
2024-07-11 16:18:45 +0500

    replace image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title('CAmpus X')
 col1,col2 = st.columns(2)
 
 with col1:
-    st.image('download.jpeg')
+    st.image('i.jpg')
 with col2:
     st.write("""
     Hi, my name is Nitish. I am an educational content creator on YouTube with 150K+ Subscribers in the domain of Data Science. I have been in the tech industry for the past 10 years and taught more than 50,000 students offline. I am passionate about data and take pride in creating content that simplifies complex topics.
